08adbae9bd63cb54e268f01bdfe1c575f5837fd9
Seonkyo Ok <uyw4687@naver.com>
2019-03-18 21:23:16 +0900

    modify

--- arch/arm64/kernel/ptree.c
+++ arch/arm64/kernel/ptree.c
@@ -1,9 +1,15 @@
-#include <arch/arm64/include/asm/unistd.h>
-#include <linux/ptree.h>
-#include <linux/sched.h>
-#include <linux/kernel.h>
+#include "../include/asm/unistd.h"
+#include <../../../include/linux/ptree.h>
+#include <../../../include/linux/sched.h>
+#include <../../../include/linux/kernel.h>
+#include <linux/uaccess.h>
+#include <linux/sched/signal.h>
+#include <linux/string.h>
+#include <linux/cred.h>
 
 int sys_ptree(struct prinfo *buf, int *nr) {
+	
+    int errno;
     
     if (buf == NULL || nr == NULL) {
         errno = EINVAL;
@@ -32,6 +38,9 @@ int sys_ptree(struct prinfo *buf, int *nr) {
 	int count=0;	
 
     struct prinfo *buf2;
+
+    struct list_head *list;
+
     buf2 = (struct prinfo *)kmalloc(sizeof(struct prinfo) * n, GFP_KERNEL);
 
     read_lock(&tasklist_lock);
@@ -44,13 +53,13 @@ int sys_ptree(struct prinfo *buf, int *nr) {
 
 	for_each_process(task){
 		if(count >= n)break;
-		buf2[count].comm = task->comm;
-		buf2[count].state = (int64_t)task->state;
+		strcpy(buf2[count].comm, task->comm);
+		buf2[count].state = (int64_t)(task->state);
 		buf2[count].pid = (pid_t)task->pid;
-		buf2[count].parent_pid = (pid_t)task->&p_pptr;
-		buf2[count].first_child_pid = (pid_t)task->&p_cptr;
-		buf2[count].next_sibling_pid = (pid_t)task->&p_osptr;
-		buf2[count].uid = (int64_t)task->uid;
+		buf2[count].parent_pid = (pid_t)task->parent->pid;
+		//buf2[count].first_child_pid = (pid_t)(list_entry(list_for_each(task->children.next, struct task_struct, children).pid);
+		//buf2[count].next_sibling_pid = (pid_t)task->&p_osptr;
+		buf2[count].uid = (int64_t)(task->cred->uid.val);
 		count++;
 	}
 	if(count<n)n=count;

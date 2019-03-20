4c03a39d556e08417fcd73062f9d82051db109bb
Seonkyo Ok <uyw4687@naver.com>
2019-03-20 19:17:40 +0900

    modify

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -6,11 +6,35 @@
 #include <linux/sched/signal.h>
 #include <linux/string.h>
 #include <linux/cred.h>
+#include <linux/list.h>
+
+void get_value(struct task_struct *task, int *count){
+	struct list_head *p;
+	p = task -> children
+	list_entry(p, struct task_struct, children);
+	(task -> children)
+}
+void assign_value(struct task_struct *task, int *count, struct prinfo *buf2) {
+		strncpy(buf2[count].comm, 64);
+		buf2[count].state = (int64_t)(task->state);
+		buf2[count].pid = (pid_t)task->pid;
+		buf2[count].parent_pid = (pid_t)task->parent->pid;
+		//buf2[count].first_child_pid = (pid_t)(list_entry(list_for_each(task->children.next, struct task_struct, children).pid);
+		//buf2[count].next_sibling_pid = (pid_t)task->&p_osptr;
+		buf2[count].uid = (int64_t)(task->cred->uid.val);
+		count++;
+	
+}
 
 long sys_ptree(struct prinfo *buf, int *nr) {
 	
-    int errno;
-    
+	int errno;
+	int n;  // the number of entries that actually copied into buf
+        struct task_struct *task;
+	int count=0;
+	struct prinfo *buf2;
+	struct list_head *list;
+
     if (buf == NULL || nr == NULL) {
         errno = EINVAL;
         return -errno;
@@ -21,7 +45,6 @@ long sys_ptree(struct prinfo *buf, int *nr) {
         return -errno;
     }
 
-    int n;  // the number of entries that actually copied into buf
     copy_from_user(&n, nr, sizeof(int));
 
     if (n < 1) {
@@ -34,34 +57,15 @@ long sys_ptree(struct prinfo *buf, int *nr) {
         return -errno;
     }
 
-	struct task_struct *task;
-	int count=0;	
-
-    struct prinfo *buf2;
-
-    struct list_head *list;
-
     buf2 = (struct prinfo *)kmalloc(sizeof(struct prinfo) * n, GFP_KERNEL);
 
     read_lock(&tasklist_lock);
 
-    /* 
-     * TODO iterate over processes with DFS algorithm, update buf2, update n
-     * discourage to use recursive function
-     * DO NOT USE sleep, kmalloc, copy_to_user, copy_from_user!
-     */
+   //  * DO NOT USE sleep, kmalloc, copy_to_user, copy_from_user!
 
 	for_each_process(task){
 		if(count >= n)break;
-		strcpy(buf2[count].comm, task->comm);
-		buf2[count].state = (int64_t)(task->state);
-		buf2[count].pid = (pid_t)task->pid;
-		buf2[count].parent_pid = (pid_t)task->parent->pid;
-		//buf2[count].first_child_pid = (pid_t)(list_entry(list_for_each(task->children.next, struct task_struct, children).pid);
-		//buf2[count].next_sibling_pid = (pid_t)task->&p_osptr;
-		buf2[count].uid = (int64_t)(task->cred->uid.val);
-		count++;
-	}
+}
 	if(count<n)n=count;
 
     /*

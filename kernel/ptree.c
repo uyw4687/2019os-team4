4298fd9596fdf0267a038df5c5ab92c236c2b8f9
Seonkyo Ok <uyw4687@naver.com>
2019-03-20 20:40:31 +0900

    recursive

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -18,32 +18,36 @@ struct prinfo pop(struct stack *st);
 void push(struct stack *st, struct prinfo p);
 struct prinfo peek(struct stack st);
 
-void get_value(struct task_struct *task, int *count, struct prinfo *buf2, int &n) {
-    struct stack st;
-    init(&st);
+//recursive
+void get_value(struct task_struct *task, int *count, struct prinfo *buf2, int *n) {
     
-    assign_value(task, count, buf2);
-    LIST_HEAD(p);
-    p = task->children;
-    list_for_each_entry(p, struct task_struct, children);
-    (task -> children) //TODO
-    while(count < n){
-        list_entry(
-        count++;
+//    struct stack st;
+    struct list_head *list;
+    
+//    init(&st);    
+    
+    if(*count > *n)
+        return;
+
+    assign_value(task, count, buf2); 
+
+    list_for_each(list, &task->children) {
+        task = list_entry(list, struct task_struct, sibling);
+        if (task->pid == 0)
+            return;
+        get_value(task, count, buf2, n);
     }
-    if(count<n)n=count; //TODO
 }
 
 void assign_value(struct task_struct *task, int *count, struct prinfo *buf2) {
-    strncpy(buf2[count].comm, 64);
+    strncpy(buf2[count].comm, task->comm, 64);
     buf2[count].state = (int64_t)(task->state);
     buf2[count].pid = (pid_t)task->pid;
     buf2[count].parent_pid = (pid_t)task->parent->pid;
     //buf2[count].first_child_pid = (pid_t)(list_entry(list_for_each(task->children.next, struct task_struct, children).pid);
     //buf2[count].next_sibling_pid = (pid_t)task->&p_osptr;
     buf2[count].uid = (int64_t)(task->cred->uid.val);
-    count++;
-    
+    *count = *count + 1;
 }
 
 long sys_ptree(struct prinfo *buf, int *nr) {
@@ -95,6 +99,9 @@ long sys_ptree(struct prinfo *buf, int *nr) {
 
     read_unlock(&tasklist_lock);
     
+    if(count < n)
+        n = count;
+
     copy_to_user(nr, &n, sizeof(int));
     copy_to_user(buf, buf2, sizeof(struct prinfo) * n);
     kfree(buf2);

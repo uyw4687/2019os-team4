47d46b9989cc4feed01260a999526d071679f201
dlrlgk4506 <dlrlgk4506@github.com>
2019-03-20 17:39:07 +0900

    modify test file

--- test/test_ptree.c
+++ test/test_ptree.c
@@ -9,6 +9,17 @@
 
 #define sys_ptree 398
 
+
+struct stack {
+	struct prinfo data[100];
+	int top;
+};
+
+void Init_stack(struct stack *st);
+void pop(struct stack *st);
+void push(struct stack *st, struct prinfo p);
+struct prinfo pick(struct stack st);
+
 int main(int argc, char *argv[]) {
 
 	int result;
@@ -18,8 +29,7 @@ int main(int argc, char *argv[]) {
 	
 	struct prinfo p;
 	
-	int i;
-
+	
 	if (argc < 2) {
 		printf("nr needed");
 		return -1;
@@ -53,11 +63,20 @@ int main(int argc, char *argv[]) {
  		perror("Bad address");
 		return -1;
 	}
+
+
+	//print ptree
+	struct stack st;
+	Init_stack(&st);
 	
-	for(i = 0 ; i < *nr ; i++) {
+	for(int i = 0 ; i < *nr ; i++) {
 		
-		p = buf[i];	
+		p = buf[i];
 		
+		if(pick(st).pid == p.pid) pop(&st);
+		else if(p.first_child_pid != 0) push(&st, p);
+		
+		for(int i=0 ; i < st.top ; i++) printf("	");
 		printf("%s,%d,%lld,%d,%d,%d,%lld\n", p.comm, p.pid, p.state, p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid);
 
 	}
@@ -65,4 +84,27 @@ int main(int argc, char *argv[]) {
 	free(nr);
 	free(buf);
 
+}
+
+void Init_stack(struct stack *st){
+	struct prinfo p;
+	p.pid = 0;
+	p.parent_pid = 0;
+	p.first_child_pid = 0;
+	p.next_sibling_pid = 0;
+	st->data[0] = p;
+	st->top = -1;
+}
+
+void push(struct stack *st, struct prinfo p){
+	st->top++;
+	st->data[st->top] = p;
+}
+
+void pop(struct stack *st){
+	st->top--;
+}
+
+struct prinfo pick(struct stack st){
+	return st.data[st.top];
 }

012319b99dbb081e9ac90f2b57d2ba2ae8cdc7b7
An Dantae <48adt@naver.com>
2019-03-20 20:50:36 +0900

    test_ptree.c modified

--- test/test_ptree.c
+++ test/test_ptree.c
@@ -11,88 +11,98 @@
 
 
 struct stack {
-	struct prinfo data[100];
-	int top;
+    struct prinfo data[100];
+    int top;
 };
+
+void init(struct stack *st);
 struct prinfo pop(struct stack *st);
 void push(struct stack *st, struct prinfo p);
 struct prinfo peek(struct stack st);
 
 int main(int argc, char *argv[]) {
 
-	int result;
+    int result;
 
-	struct prinfo *buf;
-	int *nr;
-	
-	struct prinfo p;
-	int i;
-	
-	if (argc < 2) {
-		printf("nr needed");
-		return -1;
-	}
-	
-	nr = (int*)malloc(sizeof(int));
+    struct prinfo *buf;
+    int *nr;
+    
+    struct prinfo p;
+    int i;
+    
+    if (argc < 2) {
+        printf("nr needed");
+        return -1;
+    }
+    
+    nr = (int*)malloc(sizeof(int));
 
-	if(nr != NULL)
-		*nr = atoi(argv[1]);	
-	else
-		return 1;
-	
-	buf = (struct prinfo *)malloc(sizeof(struct prinfo)*(*nr));
+    if(nr != NULL)
+        *nr = atoi(argv[1]);    
+    else
+        return 1;
+    
+    buf = (struct prinfo *)malloc(sizeof(struct prinfo)*(*nr));
 
-	if(!buf)
-		return 1;		
+    if(!buf)
+        return 1;        
 
-	errno = 0;
+    errno = 0;
 
-	result = syscall(sys_ptree, buf, nr);
+    result = syscall(sys_ptree, buf, nr);
 
-	printf("%d", result);
+    printf("%d", result);
 
-	if(result == -EINVAL) {
+    if (result == -EINVAL) {
 
-		perror("Invalid argument");
+        perror("Invalid argument");
         return -1;
 
-	} else if (result == -EFAULT) {
+    } else if (result == -EFAULT) {
 
- 		perror("Bad address");
-		return -1;
-	}
+         perror("Bad address");
+        return -1;
+    }
 
 
-	//print ptree
-	struct stack st;
-	
-	for(i = 0 ; i < *nr ; i++) {
-		
-		p = buf[i];
-		
-		if(peek(st).pid == p.next_sibling_pid) pop(&st);
-		else if(p.first_child_pid != 0) push(&st, p);
-		
-		for(int i=0 ; i < st.top ; i++) printf("	");
-		printf("%s,%d,%lld,%d,%d,%d,%lld\n", p.comm, p.pid, p.state, p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid);
+    //print ptree
+    struct stack st;
+    
+    for (i = 0; i < *nr; i++) {
+        
+        p = buf[i];
+        
+        if (peek(st).pid == p.next_sibling_pid) pop(&st);
+        else if (p.first_child_pid != 0) push(&st, p);
 
-	}
+        int j;
+        for (j = 0; j < st.top; j++) printf("\t");
+        printf("%s,%d,%lld,%d,%d,%d,%lld\n", p.comm, p.pid, p.state, p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid);
 
-	free(nr);
-	free(buf);
+    }
+
+    free(nr);
+    free(buf);
 
 }
 
-void push(struct stack *st, struct prinfo p){
-	st->top++;
-	st->data[st->top] = p;
+void init(struct stack *st) {
+    st->top = -1;
 }
 
-struct prinfo pop(struct stack *st){
-	return st->data[st->top];
-	st->top--;
+void push(struct stack *st, struct prinfo p) {
+    if (st->top >= 99) return;  // stack is full!
+    st->top++;
+    st->data[st->top] = p;
 }
 
-struct prinfo peek(struct stack st){
-	return st.data[st.top];
+struct prinfo pop(struct stack *st) {
+    if (st->top < 0) return NULL;  // stack is empty!
+    st->top--;
+    return st->data[st->top + 1];
+}
+
+struct prinfo peek(struct stack st) {
+    if (st.top < 0) return NULL;  // stack is empty!
+    return st.data[st.top];
 }

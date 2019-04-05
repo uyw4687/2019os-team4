27f504de8d995611e23d5b8bccfa5c5bd079d645
dlrlgk4506 <dlrlgk4506@github.com>
2019-04-06 02:01:39 +0900

    typo modify

--- kernel/set_rotation.c
+++ kernel/set_rotation.c
@@ -3,38 +3,35 @@
 #include <linux/module.h>
 int rotation;
 EXPORT_SYMBOL(rotation);
-int sizeq = 500;
-int queue[500];
-int frontq = 0, rearq = 0;
-EXPORT_SYMBOL(queue);
-EXPORT_SYMBOL(frontq);
-EXPORT_SYMBOL(rearq);
-EXPORT_SYMBOL(sizeq);
-
 long sys_set_rotation(int degree) {
     rotation = degree;
     return rotation;
 }
-
-int putq(int val) {
-    if( ( rearq+1 ) % sizeq == frontq ) {
+int lock_queue[500];
+EXPORT_SYMBOL(lock_queue);
+int size;
+EXPORT_SYMBOL(size);
+int front=0, rear=0, out;
+EXPORT_SYMBOL(front);
+EXPORT_SYMBOL(rear);
+EXPORT_SYMBOL(out);
+int put(int val) {
+    if( ( rear+1 ) % size == front ) {
         printk( KERN_ERR "Stack is full" );
         return -1;
     }
-    queue[rearq] = val;
-    rearq = ( rearq + 1 ) % sizeq;
+    lock_queue[rear] = val;
+    rear = ( rear + 1 ) % size;
     return val;
 }
-EXPORT_SYMBOL(putq);
-
-int getq(void) {
-    if( rearq == frontq ) {
+EXPORT_SYMBOL(put);
+int get(void) {
+    if( rear == front ) {
         printk( KERN_ERR "Stack is empty" );
         return -1;
     }
-    int a;
-    a = queue[frontq];
-    frontq = ( frontq + 1 ) % sizeq;
-    return a;
+    out = lock_queue[front];
+    front = ( front + 1 ) % size;
+    return out;
 }
-EXPORT_SYMBOL(getq);
+EXPORT_SYMBOL(get);

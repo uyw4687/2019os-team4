a3bf3b71bf01bb448fe291fa17dd00992527c5cb
dlrlgk4506 <dlrlgk4506@github.com>
2019-04-05 15:48:41 +0900

    declare global variable

--- kernel/set_rotation.c
+++ kernel/set_rotation.c
@@ -1,6 +1,10 @@
 #include "../arch/arm64/include/asm/unistd.h"
 #include <linux/kernel.h>
+#include <linux/module.h>
+int rotation;
+EXPORT_SYMBOL(rotation);
 
 long sys_set_rotation(int degree) {
-    return -1;
+    rotation = degree;
+    return rotation;
 }

7ea22c1fe08b91125543bde0a74f154f87e9af41
dlrlgk4506 <dlrlgk4506@github.com>
2019-04-05 16:51:00 +0900

    make basic queue stack

--- kernel/set_rotation.c
+++ kernel/set_rotation.c
@@ -4,7 +4,38 @@
 int rotation;
 EXPORT_SYMBOL(rotation);
 
+int queue[size];
+int frontq = 0, rearq = 0;
+int sizeq = 500;
+EXPORT_SYMBOL(queue);
+EXPORT_SYMBOL(frontq);
+EXPORT_SYMBOL(rearq);
+EXPORT_SYMBOL(sizeq);
+
 long sys_set_rotation(int degree) {
     rotation = degree;
     return rotation;
 }
+
+int putq(int val) {
+    if( ( rearq+1 ) % sizeq == front ) {
+        printk( KERN_ERR "Stack is full" );
+        return -1;
+    }
+    queue[rearq] = val;
+    rear = ( rear + 1 ) % sizeq;
+    return val;
+}
+EXPORT_SYMBOL(putq);
+
+int getq(void) {
+    if( rearq == frontq ) {
+        printk( KERN_ERR "Stack is empty" );
+        return -1;
+    }
+    int result;
+    result = queue[frontq];
+    frontq = ( frontq + 1 ) % sizeq;
+    return result;
+}
+EXPORT_SYMBOL(getq);

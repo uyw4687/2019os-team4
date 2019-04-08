579220c2a14375064a6fedfb1f29e9d6b4ba9681
An Dantae <48adt@naver.com>
2019-04-08 18:08:10 +0900

    use list_head to implement queue

--- kernel/rotation.c
+++ kernel/rotation.c
@@ -1,16 +1,37 @@
 #include "../arch/arm64/include/asm/unistd.h"
 #include <linux/kernel.h>
+#include <linux/list.h>
+#include <linux/slab.h>
+
+/* range descriptor */
+struct rd {
+    pid_t pid;      /* process id of requested process */
+    int range[2];   /* acquire lock only if range[0] <= rotation <= range[1] */
+    int state;      /* read: 0, write: 1 */
+    struct list_head list;
+};
 
 int rotation;
-int lock_queue[500];
-int size;
-int front = 0, rear = 0, out;
+struct list_head lock_queue;
+struct list_head wait_queue;
+int front = 0, rear = 0;
+int is_initialized = 0; // if initialize() is called, set to 1
+
+void initialize_list(void) {
+    // TODO concurrency, call timing
+    if (is_initialized == 0) {
+        is_initialized = 1;
+        INIT_LIST_HEAD(&lock_queue);
+        INIT_LIST_HEAD(&wait_queue);
+    }
+}
 
 long sys_set_rotation(int degree) {
-    if( degree < 0 || degree > 360) {
-        printk( KERN_ERR "Out of range" );
+    if (degree < 0 || degree > 360) {
+        printk(KERN_ERR "Out of range");
         return -1;
     }
+    initialize_list();
     rotation = degree;
     return 0;
 }
@@ -31,22 +52,28 @@ long sys_rotunlock_write(int degree, int range){
     return -1;
 }
 
-int put(int val) {
-    if ((rear + 1) % size == front) {
-        printk( KERN_ERR "Stack is full" );
+int push(struct list_head queue, struct rd* val) {
+    if (val->pid == -1) {
+        printk(KERN_ERR "invalid rd");
         return -1;
     }
-    lock_queue[rear] = val;
-    rear = (rear + 1) % size;
-    return val;
+    list_add_tail(&(val->list), &queue);
+    return 0;
 }
 
-int get(void) {
-    if (rear == front) {
-        printk( KERN_ERR "Stack is empty" );
-        return -1;
+struct rd* pop(struct list_head queue) {
+
+    struct rd* out;
+    if (queue.next == &queue) {
+        printk(KERN_ERR "queue is empty");
+
+        out = (struct rd*)kmalloc(sizeof(struct rd), GFP_KERNEL);
+        out->pid = -1;  // empty rd
+        INIT_LIST_HEAD(&(out->list));
+        return out;
     }
-    out = lock_queue[front];
-    front = (front + 1) % size;
+    out = list_entry(queue.next, struct rd, list);
+    list_del_init(queue.next);
     return out;
+    // TODO must call kfree(out);
 }

1fe0e4024a85ec6861ec37d0bf7a9ce70a0c7455
An Dantae <48adt@naver.com>
2019-04-08 16:04:29 +0900

    code readability

--- kernel/rotation.c
+++ kernel/rotation.c
@@ -2,6 +2,10 @@
 #include <linux/kernel.h>
 
 int rotation;
+int lock_queue[500];
+int size;
+int front = 0, rear = 0, out;
+
 long sys_set_rotation(int degree) {
     if( degree < 0 || degree > 360) {
         printk( KERN_ERR "Out of range" );
@@ -10,36 +14,39 @@ long sys_set_rotation(int degree) {
     rotation = degree;
     return 0;
 }
+
 long sys_rotlock_read(int degree, int range){
     return -1;
 }
+
 long sys_rotlock_write(int degree, int range){
     return -1;
 }
+
 long sys_rotunlock_read(int degree, int range){
     return -1;
 }
+
 long sys_rotunlock_write(int degree, int range){
     return -1;
 }
-int lock_queue[500];
-int size;
-int front=0, rear=0, out;
+
 int put(int val) {
-    if( ( rear+1 ) % size == front ) {
+    if ((rear + 1) % size == front) {
         printk( KERN_ERR "Stack is full" );
         return -1;
     }
     lock_queue[rear] = val;
-    rear = ( rear + 1 ) % size;
+    rear = (rear + 1) % size;
     return val;
 }
+
 int get(void) {
-    if( rear == front ) {
+    if (rear == front) {
         printk( KERN_ERR "Stack is empty" );
         return -1;
     }
     out = lock_queue[front];
-    front = ( front + 1 ) % size;
+    front = (front + 1) % size;
     return out;
 }

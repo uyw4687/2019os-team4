6fd98da069630f8ec2ae915928452830e8fbce12
Seonkyo Ok <uyw4687@naver.com>
2019-04-08 15:42:29 +0900

    selector

--- test/trial.c
+++ test/trial.c
@@ -1,13 +1,53 @@
-#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 
-void term(int signum)
+#define SYS_ROTLOCK_WRITE 400
+#define SYS_ROTUNLOCK_WRITE 402
+
+int main(int argc, char **argv)
 {
-	notFinished = 0;
-}
+	if(argc != 2)
+	{
+		printf("starting number as the only argument\n");
+		exit(1);
+	}
+	
+	char *end;
+	int value;
+	
+	value = strtol(argv[1], &end, 10);
+	
+	FILE *fp;
 
-int main()
-{
+	//writing integer input
+	//starting lock
+	syscall(SYS_ROTLOCK_WRITE, 90, 90);
 
+	fp = fopen("integer", "w");
+	fprintf(fp, "%d", value++);
+	
+	fclose(fp);
 
+	syscall(SYS_ROTUNLOCK_WRITE, 90, 90);
+	//lock ended
 
+	while(1) {
+		//writing +1
+		//starting lock
+		syscall(SYS_ROTLOCK_WRITE, 90, 90);
+	
+		fp = fopen("integer", "w");
+		fprintf(fp, "%d", value++);
+		
+		fclose(fp);
+	
+		printf("selector: %d\n", value-1);
+
+		syscall(SYS_ROTUNLOCK_WRITE, 90, 90);
+		//lock ended
+	}
+
+	return 0;
 }

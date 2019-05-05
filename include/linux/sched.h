9342ac3774f03780c30228cecfc36ea51c8e95dc
Seonkyo Ok <uyw4687@naver.com>
2019-05-05 18:40:16 +0900

    interval

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -469,8 +469,9 @@ struct sched_rt_entity {
 } __randomize_layout;
 
 struct sched_wrr_entity {
+	unsigned long			timeout;
+	unsigned int			time_slice;
     int weight;
-    unsigned int time_slice;
 }; //TODO make struct sched_wrr_entity more
 
 struct sched_dl_entity {

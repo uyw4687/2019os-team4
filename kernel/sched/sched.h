913ad5ee34cf844508ee3e4b33d04415b4c5ff7a
Seonkyo Ok <uyw4687@naver.com>
2019-05-06 21:18:39 +0900

    stop

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -512,7 +512,7 @@ struct cfs_rq {
 struct wrr_rq
 {
     unsigned int nr_running, h_nr_running;
-    struct list_head *queue;
+    struct list_head queue;
     
     struct sched_entity *curr, *next, *last, *skip;
 };

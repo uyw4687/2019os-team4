a98d253fd7fd79a38b03213954474842fe567de3
An Dantae <48adt@naver.com>
2019-05-26 23:16:14 +0900

    add get system call, test/gpsupdate.c

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -943,5 +943,6 @@ asmlinkage long sys_statx(int dfd, const char __user *path, unsigned flags,
 			  unsigned mask, struct statx __user *buffer);
 
 asmlinkage long sys_set_gps_location(struct gps_location __user *loc);
+asmlinkage long sys_get_gps_location(const char __user *pathname, struct gps_location __user *loc);
 
 #endif
--- kernel/gps.c
+++ kernel/gps.c
@@ -54,4 +54,7 @@ long sys_set_gps_location(struct gps_location __user *loc)
     return 0;
 }
 
-//long sys_get_gps_location(const char __user *pathname, struct gps_location __user *loc);
+long sys_get_gps_location(const char __user *pathname, struct gps_location __user *loc) {
+    // TODO
+    return 0;
+}
